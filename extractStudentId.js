67d9b195ab0dd8175fc63a5edd3d9c8cee8f8a83
Doug Hughes <doug@theironyard.com>
2017-02-03 15:31:23 -0500

    wip

--- extractStudentId.js
+++ extractStudentId.js
@@ -1,5 +1,5 @@
-function getIntercomData(){
-    return JSON.parse($("#IntercomSettingsScriptTag").text().match(/{.*?}/)[0]);
-}
-
-alert(getIntercomData().user_id);
+var studentId = JSON.parse($("#IntercomSettingsScriptTag").text().match(/{.*?}/)[0]).user_id;
+var command_block = $($("pre.highlight.shell code").get(0));
+var command = command_block.html();
+command = command.replace("&gt;1234&lt;", studentId);
+command_block.html(command);
