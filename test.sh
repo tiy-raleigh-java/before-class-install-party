cc3f8f75d2a9e4dac96ada6eeb4a4fb46b3a0c43
Doug Hughes <doug@theironyard.com>
2017-02-21 11:29:45 -0500

    wip

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-if ! [ xcode-select -p 2>&1 | grep CommandLineTools > /dev/null || xcode-select -p 2>&1 | grep Applications > /dev/null ] ; then
-	echo "good"
+if ! xcode-select -p 2>&1 | grep CommandLineTools > /dev/null && ! xcode-select -p 2>&1 | grep Applications > /dev/null  ; then
+	echo "nothing installed"
 else
-	echo "bad"
+	echo "something installed"
 fi
