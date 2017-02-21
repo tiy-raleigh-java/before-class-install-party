34d19c45f3ab3e988f79e30d4424af2a3e998f5d
Doug Hughes <doug@theironyard.com>
2017-02-21 11:13:28 -0500

    wip

--- install_everything.sh
+++ install_everything.sh
@@ -43,7 +43,8 @@ print PURPLE BOLD "Installing the Xcode Command Line Tools...\n"
 print "The Xcode Command Line Tools allow programmers to do command line development. We will be using some of these tools in class."
 
 # check if xcode cli tools are installed
-if ! xcode-select -p 2>&1 | grep CommandLineTools > /dev/null ; then
+if ! xcode-select -p 2>&1 | grep Applications > /dev/null ; then
+#if ! xcode-select -p 2>&1 | grep CommandLineTools > /dev/null ; then
 	# xcode cli tools are not installed
 	print "Unfortunately, we can't automate the install of the Xcode Command Line Tools for you! This script will open a dialog that reads:"
 
@@ -70,7 +71,7 @@ if ! xcode-select -p 2>&1 | grep CommandLineTools > /dev/null ; then
 	done
 	
 	# double check that the Xcode CLI tools were successfully installed
-	if xcode-select -p 2>&1 | grep CommandLineTools > /dev/null ; then
+	if ! xcode-select -p 2>&1 | grep Applications > /dev/null ; then
 		# the install was successful
 		print GREEN "Xcode Command Line Tools were successfully installed!"
  	else
--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
-echo "sdafsdfdsfdsa...."
-echo $@
-echo $1
-echo "Please enter your name:"
-read USER_NAME
-
-echo "your username: $USER_NAME"
+if ! xcode-select -p 2>&1 | grep Applications > /dev/null || ! xcode-select -p 2>&1 | grep CommandLineTools > /dev/null ; then
+	echo "good"
+else
+	echo "bad"
+fi
